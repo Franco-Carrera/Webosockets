b82726e638c7fe0979780bd6e94b99287f99ff74
Franco-Carrera <carrerafranco825@gmail.com>
2021-12-08 23:47:12 -0300

    added admin poor

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import usersRouter from "./routes/users.js";
 import upload from "./services/uploader.js";
 import __dirname from "./utils.js";
 import { Server } from "socket.io";
+import { authMiddleware } from "./utils.js";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -17,24 +18,26 @@ const server = app.listen(PORT, () => {
 });
 export const io = new Server(server);
 
-//const io = new Server(server);
-
 //handlebars
 app.engine("handlebars", engine());
 app.set("views", __dirname + "/views");
 app.set("view engine", "handlebars");
 
+const admin = true;
 //// use middlewares
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   console.log(new Date().toTimeString().split(" ")[0], req.method, req.url);
+  req.auth = admin;
   next();
 });
 app.use(express.static(__dirname + "/public"));
 app.use("/api/products", productsRouter);
 app.use("/api/users", usersRouter);
+
+//app.use("/api/carrito", carritoRouter)
 
 app.post("/api/adoption", (req, res) => {
   let userId = parseInt(req.body.uid);
@@ -67,7 +70,7 @@ app.post(
 );
 
 /////Get all Products
-app.get("/view/products", (req, res) => {
+app.get("/view/products", authMiddleware, (req, res) => {
   contenedor.getAllProducts().then((result) => {
     let info = result.payload;
     let preparedObject = {
