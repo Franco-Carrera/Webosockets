e2e4b05e9b63b5135977f412e0e909ea70f5409d
Franco-Carrera <carrerafranco825@gmail.com>
2021-12-09 18:12:30 -0300

    finallys routes with postman

--- src/routes/carrito.js
+++ src/routes/carrito.js
@@ -16,12 +16,31 @@ router.post("/", (req, res) => {
   });
 });
 
+////AGREGAR PRODUCTS AL CARRITO por id
+router.post("/:pid/products", (req, res) => {
+  let productid = parseInt(req.params.pid);
+  let cuerpo = req.body;
+  console.log(cuerpo);
+  contenedorDos.addToCart(cuerpo, productid).then((result) => {
+    res.send(result);
+  });
+});
+
 ///DELETES
 
 //VACÍA POR ID EL CARRITO
 router.delete("/:pid", (req, res) => {
   let id = parseInt(req.params.pid);
   contenedorDos.deleteCart(id).then((result) => {
+    res.send(result);
+  });
+});
+
+///// DELETE DE UN ID DE PRODUCTO DE SU ID CARRITO
+router.delete("/:pid/products/id_prod", (req, res) => {
+  let productId = parseInt(req.params.pid);
+  let idCart = parseInt(req.params.pid);
+  contenedorDos.deleteCart(productId, idCart).then((result) => {
     res.send(result);
   });
 });
@@ -33,27 +52,8 @@ router.get("/:pid/products", (req, res) => {
   let idProducts = parseInt(req.params.pid);
   contenedorDos.getAllCart(idProducts).then((result) => {
     res.send(result);
-    console.log(result.message);
+    console.log(result.payload);
   });
-});
-
-////AGREGAR PRODUCTS AL CARRITO
-router.post("/:pid/products", (req, res) => {
-  // let productid = parseInt(req.params.pid);
-  let cuerpo = req.body;
-  console.log(cuerpo);
-  contenedorDos.addToCart(cuerpo).then((result) => {
-    res.send(result);
-  });
-});
-
-///// DELETE DE UN ID DE PRODUCTO DE SU ID CARRITO
-router.delete("/:pid/products/id_prod", (req, res) => {
-  let productId = parseInt(req.params.pid);
-  let idCart = parseInt(req.params.pid);
-  // contenedor.deleteCart(productId).then((result) => {
-  //   res.send(result);
-  // });
 });
 
 ///// Reveer métodos CLASE Contenedora
