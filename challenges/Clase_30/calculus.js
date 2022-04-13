d3dd9095ee3ee36d9c4cff55ae72766c965f3fce
carrerafranco <carrerafranco825@gmail.com>
2022-04-12 23:34:38 -0300

    Desafío graphql en avance

--- challenges/Clase_30/calculus.js
+++
@@ -1,9 +0,0 @@
-let cant = parseInt(process.argv[2]) || 100000000;
-const result = {};
-
-for (let i = 0; i < cant; i++) {
-  let randomNum = Math.floor(Math.random() * 1000);
-  result[randomNum] = result[randomNum] ? result[randomNum] + 1 : 1;
-  //randomNum en array
-}
-process.send(result);
